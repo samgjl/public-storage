ff3c4c24d4d15caf63f60a0455a7aa8115c9e317
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2024-08-07 19:20:06 -0700

    dictionary update

--- dictionaries/lemmas.py
+++ dictionaries/lemmas.py
@@ -7,5 +7,6 @@ with open("lemmas.txt", "r") as f:
 with open("lemmas-clean.txt", 'w') as f, open('en_profane_words.txt', 'r') as p:
     profane = [line.strip("\n") for line in p.readlines()]
     for l in lemmas[:, 1]:
-        if len(l) > 2 and '-' not in l and l not in profane:
-            f.write(l + '\n')
+        if len(l) > 2 and l not in profane:
+            if '-' not in l and "'" not in l and '/' not in l:
+                f.write(l.upper() + '\n')
